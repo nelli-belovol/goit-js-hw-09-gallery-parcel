d2e58f1764a3a088e35bbcf33f77222617c0bb12
nelli-belovol <nelli199020@gmail.com>
2021-08-19 19:30:43 +0300

    create repo

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  lightBox: document.querySelector('.lightbox'),
+  lightBoxImage: document.querySelector('.lightbox__image'),
+  lightBoxBtn: document.querySelector("[data-action='close-lightbox']"),
+  lightboxOverlay: document.querySelector('.lightbox__overlay'),
+  gallery: document.querySelector('.js-gallery'),
+};
